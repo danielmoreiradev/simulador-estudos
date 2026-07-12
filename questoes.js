5adcdd4d6351a5c6bec54ce5fce64977b1ed3c85
Daniel Moreira <dev.danielmoreira@gmail.com>
2026-07-12 11:22:50 -0300

    Mantem banco de questoes sem conteudo demonstrativo

--- questoes.js
+++ questoes.js
@@ -3,15 +3,3 @@ const DADOS_SIMULADO = {
   identificador: "banco-pessoal-questoes-v1",
   questoes: []
 };
-
-(() => {
-  const estilo = document.createElement("link");
-  estilo.rel = "stylesheet";
-  estilo.href = "gestor-questoes.css";
-  document.head.appendChild(estilo);
-
-  const script = document.createElement("script");
-  script.src = "gestor-questoes.js";
-  script.defer = true;
-  document.head.appendChild(script);
-})();
